d8716195bb1eed3ca3e1fc03f61393168ae0104a
Alexander Loschilov <aloschil@gmail.com>
2011-10-23 20:49:05 +0400

    Added code for html and txt report representation.

--- reporter/base.py
+++ reporter/base.py
@@ -5,9 +5,12 @@ import csv
 import datetime
 from tempfile import NamedTemporaryFile
 
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.contrib.sites.models import Site
 from django.conf import settings
+from django.template import Context, loader
+
+
 
 REPORTER_FROM_EMAIL = getattr(settings, 'REPORTS_FROM_EMAIL',
                              settings.ADMINS[0][1])
@@ -86,25 +89,57 @@ class BaseReport(object):
     
     def run_report(self):
         "Run the report itself, converting the data into a csv file."
+
         w = csv.writer(self.file)
         w.writerows(self.get_data())
+
         self.file.close()
+
         if self.send:
             self.send_results()
-    
+
     def send_results(self):
-        "Send the results to the appropriate email addresses."
+        """
+        Send the results to the appropriate email addresses.
+        """
+        
         if not self.recipients:
             self.recipients = self.get_default_recipients()
-        with open(self.file.name) as f:
-            subject = self.get_email_subject()
-            message = EmailMessage(
-                subject=subject,
-                body='Please review the attached report.\n\n',
-                from_email=REPORTER_FROM_EMAIL,
-                to=self.recipients,
-                attachments=[('%s.%s.csv' % (self.name, self.date),
-                              f.read(), 'text/plain')]
-            )
-        message.send()
+        
+        template_html = 'reporter/report.html'
+
+        html = loader.get_template(template_html)
+        
+        data = self.get_data()
+        
+        subject = self.get_email_subject()
+        report_name = self.get_email_subject()
+        
+        column_headers = data[0]
+
+        d = { 'report_name': report_name,
+              'column_headers': column_headers,
+              'data': data[1:],
+              'column_count': len(column_headers)}
+        
+        html_content = html.render(Context(d))        
+       
+        column_max_lens = [max((len(str(data_line[i])) for data_line in data)) for i in xrange(len(data[0]))]
+        
+        text_content = "+" + "+".join(["-"*current_len for current_len in column_max_lens]) + "+" + "\n\r"
+        text_content += "|" + "|".join( (str(data[0][i]) + " "*(column_max_lens[i]-len(str(data[0][i])))) for i in xrange(len(data[0]))) + "|" + "\n\r"
+        text_content += "+" + "+".join(["-"*current_len for current_len in column_max_lens]) + "+" + "\n\r"
+        
+        for data_id in xrange(1,len(data)):
+            text_content += "|" + "|".join( (str(data[data_id][i]) + " "*(column_max_lens[i]-len(str(data[data_id][i])))) for i in xrange(len(data[data_id]))) + "|" + "\n\r"
+
+        text_content += "+" + "+".join(["-"*current_len for current_len in column_max_lens]) + "+" + "\n\r"
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email=REPORTER_FROM_EMAIL, to=self.recipients,
+                                     attachments=[('%s.%s.csv' % (self.name, self.date),
+                                                   open(self.file.name).read(), 'text/plain')])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        
         os.remove(self.file.name)
+        
